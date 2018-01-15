6a71e9ab987f08d35fbabed85b9ff5375f8ce1fa
nisevi <nicolas.s.vidal@gmail.com>
2018-01-15 12:59:44 +0100

    Converter working when the exchange requested is the current day.

--- lib/dollar_to_euro/exchange_rate_converter.rb
+++ lib/dollar_to_euro/exchange_rate_converter.rb
@@ -1,7 +1,17 @@
+require_relative 'dollar'
+
 class ExchangeRateConverter
   class << self
     def convert(amount, date)
-      # code for converting value here
+      parsed_date = Date.parse(date)
+      return 'Please update the database first' if database_is_out_of_date?
+      return Dollar.last.value * amount if Date.today == parsed_date
+      # TODO: consider to exclude holidays and weekends
+    end
+
+    def database_is_out_of_date?
+      # TODO: evaluate when the database is out of date
+      false
     end
   end
 end
