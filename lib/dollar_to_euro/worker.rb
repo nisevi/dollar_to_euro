b91ada3cbef9f9d6960502f68572b2958c37c280
nisevi <nicolas.s.vidal@gmail.com>
2018-01-15 12:41:57 +0100

    Logic for updating the database with the registers from the csv file..

--- lib/dollar_to_euro/worker.rb
+++ lib/dollar_to_euro/worker.rb
@@ -3,6 +3,9 @@ require 'open-uri'
 require 'csv'
 require 'mongoid'
 require 'dotenv'
+require 'digest'
+require 'byebug'
+require 'date'
 require_relative 'dollar'
 
 Sidekiq.configure_client do |config|
@@ -19,6 +22,40 @@ class UpdateDatabase
   Mongoid.load!('mongoid.yml')
 
   def perform(url)
-    # dollar = Dollar.new(date: Date.today.to_s, value: 1.123)
+    download_file(url)
+    attrs = process_csv_attributes
+    clean_attrs = setup_attributes(attrs)
+    update_database(clean_attrs)
+  end
+
+  def download_file(url)
+    IO.copy_stream(open(url), 'csv/dollars.csv')
+  end
+
+  def process_csv_attributes
+    file = File.open('csv/dollars.csv', 'r')
+    count = 0
+    attributes = []
+    while line = file.gets
+      # skip first 5 lines
+      count += 1
+      next if count < 6
+      date, value = line.split(',')
+      attributes << { date: Date.parse(date), value: value.strip.to_f }
+    end
+    attributes
+  end
+
+  def setup_attributes(attributes)
+    # if it is the first time the database is being loaded
+    return attributes.reverse if Dollar.count.zero?
+    # if the database is already up to date
+    return {} if attributes.first[:date] == Dollar.last.date
+    # we nee to load some registers to update the database
+    attributes.select { |dollar| dollar[:date] > Dollar.last.date }
+  end
+
+  def update_database(attrs)
+    Dollar.create(attrs) unless attrs.empty?
   end
 end
