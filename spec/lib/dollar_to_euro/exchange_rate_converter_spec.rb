4ebf84a5f4ee87e0a04c3d4f1738150e29daab50
nisevi <nicolas.s.vidal@gmail.com>
2018-01-16 22:36:18 +0100

    Functionality that we want to test on the exchange converter.

--- spec/lib/dollar_to_euro/exchange_rate_converter_spec.rb
+++ spec/lib/dollar_to_euro/exchange_rate_converter_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'holidays'
 
 describe 'Exchange rates functionality.' do
   before(:each) do
@@ -28,5 +27,30 @@ describe 'Exchange rates functionality.' do
     expect(ExchangeRateConverter.date_is_holiday_or_weekend?(@holiday)).to be true
     expect(ExchangeRateConverter.date_is_holiday_or_weekend?(@weekend)).to be true
     expect(ExchangeRateConverter.date_is_holiday_or_weekend?(@workday)).to be false
+  end
+
+  it 'should check if the we have registers for exchange' do
+    @dollar = Dollar.new(date: Date.today, value: 1.234)
+    @dollar.save
+    expect(Dollar.count.zero?).not_to be true
+    expect(Dollar.count).to eql 1
+    Dollar.destroy_all
+    expect(Dollar.count.zero?).to be true
+  end
+
+  it 'should return the previous available' do
+
+  end
+
+  it 'should return a default date' do
+
+  end
+
+  it 'should use a default rate' do
+
+  end
+
+  it 'should convert with the correct exchange' do
+
   end
 end
