ca8be03ac9c6a9e51956dd2d6a4a51fa82732326
nisevi <nicolas.s.vidal@gmail.com>
2018-01-16 13:48:43 +0100

    Freeze string constant.

--- lib/dollar_to_euro/ecb_url.rb
+++ lib/dollar_to_euro/ecb_url.rb
@@ -1,3 +1,3 @@
 module DollarToEuro
-  ECB_URL = 'http://sdw.ecb.europa.eu/quickviewexport.do?SERIES_KEY=120.EXR.D.USD.EUR.SP00.A'
+  ECB_URL = 'http://sdw.ecb.europa.eu/quickviewexport.do?SERIES_KEY=120.EXR.D.USD.EUR.SP00.A'.freeze
 end
