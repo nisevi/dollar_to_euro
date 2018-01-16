aa66fbd15158a6eabf1b8359ec67613bff255488
nisevi <nicolas.s.vidal@gmail.com>
2018-01-16 22:23:03 +0100

    Added tests for files that are being required to make this app work.

--- spec/lib/dollar_to_euro_spec.rb
+++ spec/lib/dollar_to_euro_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
-describe 'My behaviour' do
-  it 'should do something' do
-    expect(true).to be true
+describe 'Check if file exists and requires all necessary files.' do
+  it 'should be a file' do
+    expect(File).to exist('lib/dollar_to_euro.rb')
+  end
+
+  it 'should require all necessary files' do
+    directory = Dir['lib/dollar_to_euro/*']
+    files = directory.map! { |path| path[/[\w-]+?(?=\.)/] }
+    file = File.open('lib/dollar_to_euro.rb')
+    file.each do |line|
+      expect(files).to include(line[/(?<=\/)[^\']+/])
+    end
   end
 end
