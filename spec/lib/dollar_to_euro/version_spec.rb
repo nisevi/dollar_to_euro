51b22b0c21f02894efc5a5bb253e73e25ed545cb
nisevi <nicolas.s.vidal@gmail.com>
2018-01-16 12:43:52 +0100

    Test app version.

--- spec/lib/dollar_to_euro/version_spec.rb
+++ spec/lib/dollar_to_euro/version_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
+require 'byebug'
 
-describe 'My behaviour' do
-  it 'should do something' do
-    expect(true).to be true
+describe 'Check app version.' do
+  it 'should be the latest version' do
+    expect(DollarToEuro::VERSION).to match('0.1.0')
   end
 end
