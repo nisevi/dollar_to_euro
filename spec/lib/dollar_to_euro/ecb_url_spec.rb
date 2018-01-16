1bdb258c893a3ccf79612be25a570ef2232c0a99
nisevi <nicolas.s.vidal@gmail.com>
2018-01-16 13:49:14 +0100

    Tests for the source url.

--- spec/lib/dollar_to_euro/ecb_url_spec.rb
+++ spec/lib/dollar_to_euro/ecb_url_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 
-describe 'My behaviour' do
-  it 'should do something' do
-    expect(true).to be true
+describe 'Source URL from where exchange values are collected.' do
+  it 'should be a string' do
+    expect(DollarToEuro::ECB_URL).to be_kind_of(String)
+  end
+
+  it 'should be an URL' do
+    @url = URI.parse(DollarToEuro::ECB_URL) rescue false
+    expect(@url).not_to be false
+    expect(@url).to be_kind_of URI::HTTP
+    expect(@url.host).to match /sdw\.ecb\.europa\.eu/
+    expect(@url.html_safe?).to be false
+    expect(@url.to_s).to match DollarToEuro::ECB_URL
   end
 end
