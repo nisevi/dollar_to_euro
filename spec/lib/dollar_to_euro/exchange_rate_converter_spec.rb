0ceaf9ff1bf5f9cfcb5f985c390b74ff7e9452e9
nisevi <nicolas.s.vidal@gmail.com>
2018-01-16 14:17:44 +0100

    Added tests for three exchange rate methods.

--- spec/lib/dollar_to_euro/exchange_rate_converter_spec.rb
+++ spec/lib/dollar_to_euro/exchange_rate_converter_spec.rb
@@ -1,7 +1,32 @@
 require 'spec_helper'
+require 'holidays'
 
-describe 'My behaviour' do
-  it 'should do something' do
-    expect(true).to be true
+describe 'Exchange rates functionality.' do
+  before(:each) do
+    @weekend = '2018-01-14'
+    @holiday = '2018-01-15'
+    @workday = '2018-01-16'
+  end
+
+  it 'should be holidays' do
+    expect(ExchangeRateConverter.holiday?(@holiday)).to be true
+  end
+
+  it 'should not be holidays' do
+    expect(ExchangeRateConverter.holiday?(@workday)).to be false
+  end
+
+  it 'should be weekend' do
+    expect(ExchangeRateConverter.weekend?(@weekend)).to be true
+  end
+
+  it 'should not be a weekend' do
+    expect(ExchangeRateConverter.weekend?(@workday)).to be false
+  end
+
+  it 'should check holiday or weekend' do
+    expect(ExchangeRateConverter.date_is_holiday_or_weekend?(@holiday)).to be true
+    expect(ExchangeRateConverter.date_is_holiday_or_weekend?(@weekend)).to be true
+    expect(ExchangeRateConverter.date_is_holiday_or_weekend?(@workday)).to be false
   end
 end
