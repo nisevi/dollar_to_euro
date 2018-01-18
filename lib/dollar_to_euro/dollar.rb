129b38bbc37f554878a28b49c947ea9673e58a3c
nisevi <nicolas.s.vidal@gmail.com>
2018-01-18 13:01:53 +0100

    Added comment to Dollar class.

--- lib/dollar_to_euro/dollar.rb
+++ lib/dollar_to_euro/dollar.rb
@@ -1,3 +1,8 @@
+##
+# Mongoid by default stores documents in a collection that is the pluralized
+# form of the class name. For the following Dollar class, the collection the
+# document would get stored in would be named dollars.
+
 class Dollar
   include Mongoid::Document
   field :date, type: Date
