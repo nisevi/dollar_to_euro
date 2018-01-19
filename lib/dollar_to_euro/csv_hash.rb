fb792e933c45b670eb29353565059ec8aaa07112
nisevi <nicolas.s.vidal@gmail.com>
2018-01-19 12:06:33 +0100

    Missing magic comment # frozen_string_literal: true

--- lib/dollar_to_euro/csv_hash.rb
+++ lib/dollar_to_euro/csv_hash.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 ##
 # Mongoid by default stores documents in a collection that is the pluralized
 # form of the class name. For the following CsvHash class, the collection the
 # document would get stored in would be named csv_hashes.
-
 class CsvHash
   include Mongoid::Document
   include Mongoid::Timestamps::Created
--- lib/dollar_to_euro/dollar.rb
+++ lib/dollar_to_euro/dollar.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 ##
 # Mongoid by default stores documents in a collection that is the pluralized
 # form of the class name. For the following Dollar class, the collection the
 # document would get stored in would be named dollars.
-
 class Dollar
   include Mongoid::Document
   field :date, type: Date
