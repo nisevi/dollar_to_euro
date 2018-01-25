99b0d0ed9decc6012eb23611eaf541d763ddb84f
nisevi <nicolas.s.vidal@gmail.com>
2018-01-25 17:04:35 +0100

    No more holidays.

--- lib/dollar_to_euro/exchange_rate_converter.rb
+++ lib/dollar_to_euro/exchange_rate_converter.rb
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'dollar'
-require 'holidays'
-require 'holidays/core_extensions/date'
-
-# Date class is opened here to include the Holidays methods.
-class Date
-  include Holidays::CoreExtensions::Date
-end
 
 # ExchangeRateConverter class holds all the logic to make the conversion.
 class ExchangeRateConverter
@@ -18,14 +11,14 @@ class ExchangeRateConverter
       return calculate_with_default_rate(amount, parsed_date) if default_date?(parsed_date)
       # if exchange rate exists for the given date return the exchange
       register = Dollar.where(date: parsed_date)
-      return register.first.value / amount if register.exists?
-      # if the date is holiday or weekend pick the previous available rate exchange
-      previous_rate_available(parsed_date) / amount if date_is_holiday_or_weekend?(date)
+      return amount / register.first.value if register.exists?
+      # if the date doesn't exist in our database use the previous date available
+      amount / previous_rate_available(parsed_date)
     end
 
     def calculate_with_default_rate(amount, parsed_date)
-      return Dollar.last.value / amount if parsed_date >= Date.today
-      Dollar.first.value / amount
+      return amount / Dollar.last.value if parsed_date >= Date.today
+      amount / Dollar.first.value
     end
 
     # returns true if date is oldest than the oldest register we have
@@ -36,21 +29,6 @@ class ExchangeRateConverter
 
     def previous_rate_available(date)
       Dollar.where(:date.lte => date).last.value
-    end
-
-    def date_is_holiday_or_weekend?(date)
-      weekend?(date) || holiday?(date)
-    end
-
-    def holiday?(date)
-      Holidays.cache_between(Time.now, 2.years.from_now, :us, :observed)
-      year, month, day = date.split '-'
-      parsed_date = Date.civil(year.to_i, month.to_i, day.to_i)
-      parsed_date.holiday?(:us)
-    end
-
-    def weekend?(date)
-      [6, 7].include?(Date.parse(date).cwday)
     end
 
     def database_is_out_of_date?
