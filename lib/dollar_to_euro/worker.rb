4a6743f713f8b49d9ea9c980f49c3fa63f776b98
nisevi <nicolas.s.vidal@gmail.com>
2018-01-25 16:18:13 +0100

    Sidekiq is not used anymore.

--- lib/dollar_to_euro/worker.rb
+++ lib/dollar_to_euro/worker.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'sidekiq'
 require 'open-uri'
 require 'mongoid'
 require 'digest/sha2'
@@ -8,62 +7,55 @@ require 'date'
 require_relative 'dollar'
 require_relative 'csv_hash'
 
-Sidekiq.configure_client do |config|
-  config.redis = { db: 1 }
-end
-
-Sidekiq.configure_server do |config|
-  config.redis = { db: 1 }
-end
-
 # Worker that updates the database based on the downloaded CSV file.
 class UpdateDatabase
-  include Sidekiq::Worker
   Mongoid.load!('mongoid.yml', :development)
 
-  def perform(url)
-    download_file(url)
-    digest = Digest::SHA2.new(256).hexdigest('csv/dollars.csv')
-    return if file_not_changed?(digest)
-    CsvHash.create(digest: digest)
-    clean_attrs = setup_attributes(process_csv_attributes)
-    update_database(clean_attrs)
-  end
-
-  def download_file(url)
-    IO.copy_stream(open(url), 'csv/dollars.csv')
-  end
-
-  def file_not_changed?(digest)
-    last = CsvHash.last
-    !last.nil? && last.digest == digest
-  end
-
-  def process_csv_attributes
-    file = File.open('csv/dollars.csv', 'r')
-    count = 0
-    attributes = []
-    while line = file.gets
-      # skip first 5 lines
-      count += 1
-      next if count < 6
-      date, value = line.split(',')
-      attributes << { date: Date.parse(date), value: value.strip.to_f }
+  class << self
+    def perform(url)
+      download_file(url)
+      digest = Digest::SHA2.new(256).hexdigest('csv/dollars.csv')
+      return if file_not_changed?(digest)
+      CsvHash.create(digest: digest)
+      clean_attrs = setup_attributes(process_csv_attributes)
+      update_database(clean_attrs)
     end
-    attributes
-  end
 
-  def setup_attributes(attributes)
-    # if it is the first time the database is being loaded
-    return attributes.reverse if Dollar.count.zero?
-    last = Dollar.last
-    # if the database is already up to date
-    return {} if attributes.first[:date] == last.date
-    # we nee to load some registers to update the database
-    attributes.select { |dollar| dollar[:date] > last.date }.reverse
-  end
+    def download_file(url)
+      IO.copy_stream(open(url), 'csv/dollars.csv')
+    end
 
-  def update_database(attrs)
-    Dollar.create(attrs) unless attrs.empty?
+    def file_not_changed?(digest)
+      last = CsvHash.last
+      !last.nil? && last.digest == digest
+    end
+
+    def process_csv_attributes
+      file = File.open('csv/dollars.csv', 'r')
+      count = 0
+      attributes = []
+      while line = file.gets
+        # skip first 5 lines
+        count += 1
+        next if count < 6
+        date, value = line.split(',')
+        attributes << { date: Date.parse(date), value: value.strip.to_f }
+      end
+      attributes
+    end
+
+    def setup_attributes(attributes)
+      # if it is the first time the database is being loaded
+      return attributes.reverse if Dollar.count.zero?
+      last = Dollar.last
+      # if the database is already up to date
+      return {} if attributes.first[:date] == last.date
+      # we nee to load some registers to update the database
+      attributes.select { |dollar| dollar[:date] > last.date }.reverse
+    end
+
+    def update_database(attrs)
+      Dollar.create(attrs) unless attrs.empty?
+    end
   end
 end
