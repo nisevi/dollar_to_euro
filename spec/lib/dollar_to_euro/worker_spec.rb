b9386719471cb42194d2f483d5c9ef2acdd7b39c
nisevi <nicolas.s.vidal@gmail.com>
2018-01-25 23:38:59 +0100

    Pending tests.

--- spec/lib/dollar_to_euro/worker_spec.rb
+++ spec/lib/dollar_to_euro/worker_spec.rb
@@ -1,23 +1,15 @@
 require 'spec_helper'
 
 describe 'Worker is being processed to update the database.' do
-  it 'should enqueue a job' do
-    expect(true).to be true
-  end
+  it 'should perform the update'
 
-  it 'should download the file' do
-    expect(true).to be true
-  end
+  it 'should download the file'
 
-  it 'should process the CSV file' do
-    expect(true).to be true
-  end
+  it 'should tell us if the file has changed'
 
-  it 'should setup the attributes' do
-    expect(true).to be true
-  end
+  it 'should process the CSV file'
 
-  it 'should update the database' do
-    expect(true).to be true
-  end
+  it 'should setup the attributes'
+
+  it 'should update the database'
 end
