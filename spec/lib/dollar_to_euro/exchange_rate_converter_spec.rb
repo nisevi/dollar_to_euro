0bd6fa085fa27eb02017f4b2f195e0e989cc7e8e
nisevi <nicolas.s.vidal@gmail.com>
2018-01-25 17:04:05 +0100

    Holidays was removed since if the date is not in the database we are using the previous date available.

--- spec/lib/dollar_to_euro/exchange_rate_converter_spec.rb
+++ spec/lib/dollar_to_euro/exchange_rate_converter_spec.rb
@@ -1,35 +1,5 @@
 require 'spec_helper'
 
-describe 'Exchange rates functionality.' do
-  before(:each) do
-    @weekend = '2018-01-14'
-    @holiday = '2018-01-15'
-    @workday = '2018-01-16'
-  end
-
-  it 'should be holidays' do
-    expect(ExchangeRateConverter.holiday?(@holiday)).to be true
-  end
-
-  it 'should not be holidays' do
-    expect(ExchangeRateConverter.holiday?(@workday)).to be false
-  end
-
-  it 'should be weekend' do
-    expect(ExchangeRateConverter.weekend?(@weekend)).to be true
-  end
-
-  it 'should not be a weekend' do
-    expect(ExchangeRateConverter.weekend?(@workday)).to be false
-  end
-
-  it 'should check holiday or weekend' do
-    expect(ExchangeRateConverter.date_is_holiday_or_weekend?(@holiday)).to be true
-    expect(ExchangeRateConverter.date_is_holiday_or_weekend?(@weekend)).to be true
-    expect(ExchangeRateConverter.date_is_holiday_or_weekend?(@workday)).to be false
-  end
-end
-
 describe 'Methods that involve database interaction' do
   before(:each) do
     @dollar1 = Dollar.create(date: Date.parse('2017-12-27'), value: rand(0.1...1.9))
