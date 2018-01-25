c07aa7de0c906fd6accc9954bb355e4f23c4959d
nisevi <nicolas.s.vidal@gmail.com>
2018-01-25 16:14:47 +0100

    Refactor because of redundant expression.

--- spec/lib/dollar_to_euro_spec.rb
+++ spec/lib/dollar_to_euro_spec.rb
@@ -10,7 +10,7 @@ describe 'Check if file exists and requires all necessary files.' do
     files = directory.map! { |path| path[/[\w-]+?(?=\.)/] }
     file = File.open('lib/dollar_to_euro.rb')
     file.each do |line|
-      expect(files).to include(line[/(?<=\/)[^\']+/])
+      expect(files).to include(line[/(?<=\/)[^']+/])
     end
   end
 end
