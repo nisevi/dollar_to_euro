ebf2e0095b40f830d9e76697749e0cad78b51d3b
nisevi <nicolas.s.vidal@gmail.com>
2018-02-19 10:49:49 +0100

    Removing code smells.

--- lib/dollar_to_euro/exchange_rate_converter.rb
+++ lib/dollar_to_euro/exchange_rate_converter.rb
@@ -2,17 +2,13 @@
 
 require_relative 'dollar'
 
-# ExchangeRateConverter class holds all the logic to make the conversion.
 class ExchangeRateConverter
   class << self
     def convert(amount, date)
       parsed_date = Date.parse(date)
-      # if converting at oldest or latest rate
       return calculate_with_default_rate(amount, parsed_date) if default_date?(parsed_date)
-      # if exchange rate exists for the given date return the exchange
       register = Dollar.where(date: parsed_date)
       return amount / register.first.value if register.exists?
-      # if the date doesn't exist in our database use the previous date available
       amount / previous_rate_available(parsed_date)
     end
 
@@ -21,8 +17,6 @@ class ExchangeRateConverter
       amount / Dollar.first.value
     end
 
-    # returns true if date is oldest than the oldest register we have
-    # returns true if date is today's date
     def default_date?(parsed_date)
       parsed_date >= Date.today || parsed_date < Dollar.first.date
     end
@@ -32,7 +26,6 @@ class ExchangeRateConverter
     end
 
     def database_is_out_of_date?
-      # TODO: handle more complex scenarios
       Dollar.count.zero?
     end
   end
